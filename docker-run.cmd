b161a340435538a29b8215993eb5876b73f4753c
Christhian <peralta6998@gmail.com>
2025-10-29 01:52:04 -0500

    Correccion del nombre de red

--- docker-run.cmd
+++ docker-run.cmd
@@ -2,4 +2,4 @@ REM --- Construir imagen ---
 docker build -t proyectoDAWII/frontend:1.0 -f Dockerfile .
 
 REM --- Crear contenedor ---
-docker run -d --name frontend --network proyectoDAWII-net -p 4201:80 proyectoDAWII/frontend:1.0
+docker run -d --name frontend --network proyectoDAWII -p 4201:80 proyectoDAWII/frontend:1.0
